f67d36244380dbe226c76c87b9fbc8256f7607a3
MaximeWishnevski <velolivefan@gmail.com>
2022-05-29 16:37:40 +0300

    add 2,3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay ({seconds}) {
+    localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+setCurrentTime()
+
+function setCurrentTime(){
+    if(!localStorage.getItem(STORAGE_KEY)){
+        return
+    }
+    player.setCurrentTime(localStorage.getItem(STORAGE_KEY))
+}
